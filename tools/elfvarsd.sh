64f19e9e5b6e129a0d976f9d9589d7052d565c6a
Bernhard Heinloth <heinloth@cs.fau.de>
2022-12-07 18:11:55 +0100

    elfvars daemon wrapper script

---
+++ tools/elfvarsd.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ELFVARSARGS=( '-d' '-D' '-t' '-w' '-p' )
+CACHEPREFIX=".elfvarsd-cache"
+DEFAULTSOCKET="0.0.0.0:9001"
+
+BEAN_TOOLS=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")" )
+
+declare -a BASEDIRS
+while [[ $# -gt 0 ]] ; do
+	case "$1" in
+		-h)
+			echo "Inspect (analyze) all files in base directories on start, cache the results and start elfvars as daemon"
+			echo
+			echo "	Usage: $0 [-h] [-c CACHEPREFIX] [SOCKET [BASE DIRs]] [-- ELFVARS-ARGS]"
+			echo
+			echo "Default cache prefix is '${CACHEPREFIX}'"
+			echo "Default socket is '${DEFAULTSOCKET}'"
+			echo "Default ELFVARS-ARGS are '${ELFVARSARGS[@]}'"
+			exit 0
+			;;
+
+		-c)
+			shift
+			CACHEPREFIX=$1
+			;;
+		--)
+			shift
+			ELFVARSARGS=$@
+			break
+			;;
+		*)
+			if [[ -z "${SOCKET+empty}" ]]; then
+				SOCKET=$1
+			else
+				BASEDIRS+=( $1 )
+			fi
+			;;
+	esac
+	shift
+done
+
+ELFVARSARGS+=( '-c' "${CACHEPREFIX}" )
+
+echo "Preprocessing (caching) ${#BASEDIRS[@]} directories..." >&2
+for BASEDIR in "${BASEDIRS[@]}" ; do
+	FILES=( $(find -L ubuntu-focal/zlib1g -type f -name '*.so*' -exec file -L {} + | grep "shared object" | sed 's/: .*//') )
+	echo "Analyzing ${#FILES[@]} files in ${BASEDIR}..." >&2
+	${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]} -r "${BASEDIR}" "${FILES[@]}"
+done
+
+
+ELFVARSARGS+=( '-s' "${SOCKET:=$DEFAULTSOCKET}" )
+echo "Starting elfvars with '${ELFVARSARGS[@]}' "
+exec ${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]}
