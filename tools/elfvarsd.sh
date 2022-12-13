9113e420644eecbb54bc628c8adcfa4082ff77b7
Bernhard Heinloth <heinloth@cs.fau.de>
2022-12-13 18:58:00 +0100

    Fix/extend elfvarsd caching

--- tools/elfvarsd.sh
+++ tools/elfvarsd.sh
@@ -14,6 +14,9 @@ while [[ $# -gt 0 ]] ; do
 			echo
 			echo "	Usage: $0 [-h] [-c CACHEPREFIX] [SOCKET [BASE DIRs]] [-- ELFVARS-ARGS]"
 			echo
+			echo "A socket value 'cache' will only cache the base dirs,"
+			echo "but not open a listen socket."
+			echo
 			echo "Default cache prefix is '${CACHEPREFIX}'"
 			echo "Default socket is '${DEFAULTSOCKET}'"
 			echo "Default ELFVARS-ARGS are '${ELFVARSARGS[@]}'"
@@ -44,12 +47,14 @@ ELFVARSARGS+=( '-c' "${CACHEPREFIX}" )
 
 echo "Preprocessing (caching) ${#BASEDIRS[@]} directories..." >&2
 for BASEDIR in "${BASEDIRS[@]}" ; do
-	FILES=( $(find -L ubuntu-focal/zlib1g -type f -name '*.so*' -exec file -L {} + | grep "shared object" | sed 's/: .*//') )
+	FILES=( $(find -L "${BASEDIR}" -type f -name '*.so*' -exec file -L {} + | grep "shared object" | sed 's/: .*//') )
 	echo "Analyzing ${#FILES[@]} files in ${BASEDIR}..." >&2
-	${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]} -r "${BASEDIR}" "${FILES[@]}"
+	${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]} -b "${BASEDIR}" "${FILES[@]}"
 done
 
 
 ELFVARSARGS+=( '-s' "${SOCKET:=$DEFAULTSOCKET}" )
-echo "Starting elfvars with '${ELFVARSARGS[@]}' "
-exec ${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]}
+if [[ "${SOCKET,,}" != "cache" ]] ; then
+	echo "Starting elfvars with '${ELFVARSARGS[@]}' "
+	exec ${BEAN_TOOLS}/elfvars.py ${ELFVARSARGS[@]}
+fi
